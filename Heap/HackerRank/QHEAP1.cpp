26773fd34dd718e92ce23c86800b3921305c67fc
Mir00r <abdur.razzaq06@gmail.com>
2016-03-26 16:38:49 +0600

    Add another way to insert, delete and display the number in heap manually

--- Heap/HackerRank/QHEAP1.cpp
+++ Heap/HackerRank/QHEAP1.cpp
@@ -19,8 +19,84 @@ using namespace std;
 #define mod 7477777
 #define PI 3.1415926535897932384626433832795
 
-vector < LI > vt;
+// Way 1
 
+void MinHeapify( LLI* heap, int HeapSize, int index) {
+    int tmp, left, right;
+
+    if ( index >= HeapSize )
+        return;
+    tmp = index;
+    left = 2*tmp+1;
+    right = 2*tmp+2;
+
+    if ( left <= HeapSize && heap[ left ] < heap[ tmp ] )
+        tmp = left;
+    if ( right <= HeapSize && heap[ right ] < heap[ tmp ] )
+        tmp = right;
+    if ( tmp == index )
+        return;
+    swap( heap[ tmp ], heap[ index ] );
+    MinHeapify(heap, HeapSize, tmp);
+    return;
+}
+
+
+void DeleteElement( LLI* heap, int& HeapSize, int val) {
+    int index = -1, i;
+    for ( i = 0; i <= HeapSize; i++) {
+        if (heap[i] == val) {
+            index = i;
+            break;
+        }
+    }
+    swap( heap[ HeapSize ], heap[ index ] );
+    HeapSize--;
+    MinHeapify( heap, HeapSize, index );
+    return;
+}
+
+
+int main() {
+    int queries, i;
+    int qType;
+    int HeapSize = -1;
+
+    scanf ("%d", &queries);
+    LLI heap[queries];
+    LLI num;
+
+    for ( i = 0; i < queries; i++) {
+        scanf ("%d", &qType);
+        if (qType == 1) {
+            scanf ("%lld", &num);
+            HeapSize++;
+            int tmp = HeapSize;
+            heap[ tmp ] = num;
+            int parent = ( tmp - 1) / 2;
+
+            while ( parent >= 0 && heap[ parent ] > heap[ tmp ] ) {
+                swap( heap[ parent ], heap[ tmp ] );
+                tmp = parent;
+                parent = (tmp - 1) / 2;
+            }
+        }
+        else if (qType == 2) {
+            scanf ("%lld", &num);
+            DeleteElement(heap, HeapSize, num);
+        }
+        else if (qType == 3)
+            printf ("%lld\n", heap[ 0 ]);
+    }
+    return 0;
+}
+
+
+
+
+
+// Way 2
+vector < LI > vt;
 
 int main() {
     LLI query, opa, num;
